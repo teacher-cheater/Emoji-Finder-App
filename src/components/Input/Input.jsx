d56ca3ae761b464b98ee47803199172edca51e82
teacher_cheater <johnnykayfun@gmail.com>
2022-07-21 15:57:43 +0300

    fix bugs for search emoji

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,13 @@
-export function Input() {
+export function Input({ input, setInput }) {
   return (
     <div className="main__search">
       <input
+        value={input}
         id="main__input"
         className="main__input"
         placeholder="emoji search"
         type="text"
+        onChange={(event) => setInput(event.target.value)}
       />
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,11 @@
 import { Card } from "../Card/Card.jsx";
 import { Input } from "../Input/Input.jsx";
 import { data } from "../../data";
-
+import { useState } from "react";
 //нужно перебрать keywords и убрать повторяющиеся элементы(использовать set и преобразовать в строку и обратно)
 
-function getUniqKeywords(data) {
-  return data.map((elem) => {
+function getUniqKeywords(dataKey) {
+  return dataKey.map((elem) => {
     return {
       ...elem,
       keywords: [...new Set(elem.keywords.split(" "))].join(" "),
@@ -15,19 +15,24 @@ function getUniqKeywords(data) {
 const uniqKey = getUniqKeywords(data);
 
 export function Main() {
+  const [input, setInput] = useState("");
   return (
     <main className="main">
       <div className="main__container">
-        <Input />
+        <Input setInput={setInput} input={input} />
         <div className="main__content">
-          {uniqKey.map((elem, index) => (
-            <Card
-              key={index}
-              title={elem.symbol}
-              symbol={elem.title}
-              keywords={elem.keywords}
-            />
-          ))}
+          {uniqKey
+            .filter((elem) =>
+              elem.keywords.toLowerCase().includes(input.trim().toLowerCase())
+            )
+            .map((elem, index) => (
+              <Card
+                key={index}
+                title={elem.symbol}
+                symbol={elem.title}
+                keywords={elem.keywords}
+              />
+            ))}
         </div>
       </div>
     </main>
