ba8d6ff44a5ee42ab293c4d14f93040cca5b446b
teacher_cheater <johnnykayfun@gmail.com>
2022-07-22 14:34:02 +0300

    other search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-//{data.map((elem, index) => (
-//<div className="main__cart">
-//  <>
-//    <div key={index} className="main__picture">
-//      {elem.title}
-//    </div>
-//    <p key={index} className="main__title">
-//      {elem.symbol}
-//    </p>
-//    <p key={index} className="main__text">
-//      {elem.keywords}
-//    </p>
-//  </>
-//  //</div>
-//))}
-
 import { Header } from "./components/Header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import { Main } from "./components/Main/Main.jsx";
 
+
 function App() {
+
+  //const fuse = new Fuse(data, {
+  //  keys: ["title", "keywords"],
+  //});
+  //console.log(fuse)
+
+  //const results = fuse.search('100');
+  //const titleResults = results.map(result => result.item)
   return (
     <div className="wrapper">
       <Header />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import { Card } from "../Card/Card.jsx";
 import { Input } from "../Input/Input.jsx";
 import { data } from "../../data";
 import { useState } from "react";
-//нужно перебрать keywords и убрать повтор-яющиеся элементы(использовать set и преобразовать в строку и обратно)
+import Fuse from "fuse.js";
 
 function getUniqKeywords(dataKey) {
   return dataKey.map((elem) => {
@@ -16,29 +16,33 @@ const uniqKey = getUniqKeywords(data);
 
 export function Main() {
   const [input, setInput] = useState("");
-  const inputValue = input.split(" ").filter((elem) => elem.trim());
+  //const inputValue = input.split(" ").filter((elem) => elem.trim());
+
+  const fuse = new Fuse(uniqKey, {
+    keys: ["title", "keywords"],
+  });
+
+  const results = fuse.search(input);
+  const titleResults = input ? results.map((elem) => elem.item) : uniqKey;
+
+  //function setInputs({ currentTarget = {} }) {
+  //  const { value } = currentTarget;
+  //  setInput(value);
+  //}
 
   return (
     <main className="main">
       <div className="main__container">
         <Input setInput={setInput} input={input} />
         <div className="main__content">
-          {uniqKey
-            .filter((elem) =>
-              inputValue.every(
-                (elemSearch) =>
-                  elem.keywords.toLowerCase().includes(elemSearch) ||
-                  elem.title.toLowerCase().includes(elemSearch)
-              )
-            )
-            .map((elem, index) => (
-              <Card
-                key={index}
-                title={elem.symbol}
-                symbol={elem.title}
-                keywords={elem.keywords}
-              />
-            ))}
+          {titleResults.map((elem, index) => (
+            <Card
+              key={index}
+              title={elem.symbol}
+              symbol={elem.title}
+              keywords={elem.keywords}
+            />
+          ))}
         </div>
       </div>
     </main>
